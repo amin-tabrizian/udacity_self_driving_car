ef2ec3b02a3d64654f47e456f27d7ecd7d16a7f9
amin-tabrizian <amin_tabrizian@live.com>
2023-01-14 12:16:23 -0500

    print path added

--- part3_11.py
+++ part3_11.py
@@ -1,19 +1,23 @@
-import heapq
+import numpy as np
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
         [0, 0, 1, 0, 1, 0],
         [0, 0, 1, 0, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+        [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 print(goal)
 
-delta = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+delta = [[-1, 0],
+         [0, -1],
+         [1, 0],
+         [0, 1]]
+delta_name = ['^', '<', 'v', '>']
 cost = 1
 
 def is_valid(row, column, grid):
-    if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[0]):
+    if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[0]) or grid[row][column]:
         return False
     return True
 
@@ -24,34 +28,63 @@ def is_repeated(node, visited_list):
     return False
 
 class Search:
-    def __init__(self, grid, init, goal, delta, cost):
+    def __init__(self, grid, init, goal, delta, cost, delta_name):
         self.grid = grid
         self.init = init
         self.goal = goal
         self.delta = delta
         self.cost = cost
-        self.open_list = []
-        heapq.heappush(self.open_list, (0, [0, 0, 0]))
+        self.delta_name = delta_name
+        self.open_list = [[0, 0, 0]]
         self.visited_list = []
-
+        self.expansion_grid = -1*np.ones((len(grid), len(grid[0])))
+        self.step = 0
     def expand(self, node):
         for action in delta:
             i = action[0] + node[1]
             j = action[1] + node[2]
-            if is_valid(i, j, self.grid) and not is_repeated([0, i, j], self.visited_list) and not self.grid[i][j]:
+            if is_valid(i, j, self.grid) and not is_repeated([0, i, j], self.visited_list):
                 new = [node[0] + 1, i, j]
-                if not is_repeated(new, self.visited_list):
-                    heapq.heappush(self.open_list, (new[0], new))
-
-    def compute(self):
-        while self.open_list:
-            node = heapq.heappop(self.open_list)[1]
-            if node[1:3] == self.goal:
-                print("final", node)
-                return
-            self.visited_list.append(node)
+                if not is_repeated(new, self.open_list):
+                    self.open_list.append(new)
+        # print("Open list updated:")
+        # print(self.open_list)        
+        self.expansion_grid[node[1], node[2]] = self.step
+        self.step += 1
+        # print(self.step)
+    def compute_policy(self):
+        policy = np.chararray((len(self.grid), len(self.grid[0])))
+        policy[:] =''
+        for i in range(len(self.grid)):
+            max = 0
+            for j in range(len(self.grid[0])):
+                for idx, action in enumerate(delta):
+                    if is_valid(i, j, self.grid):
+                       new_i =  action[0] + i
+                       new_j = action[1] + j
+                    if is_valid(new_i, new_j, self.grid):
+                        if max <= self.expansion_grid[new_i, new_j]:
+                            policy[i, j] = self.delta_name[idx]
+                            max = self.expansion_grid[new_i, new_j]
+        return policy
+    def compute(self, node = [0, 0, 0]):
+        self.open_list.remove(node)
+        self.visited_list.append(node)
+        while  node[1:3] != self.goal:
             self.expand(node)
-        print("Fail")
-        
-a = Search(grid, init, goal, delta, cost)
+            self.open_list = sorted(self.open_list, key=lambda t: t[0])
+            if self.open_list == []:
+                print("Fail")
+                return
+            node = self.open_list[0]
+            self.open_list.remove(node)
+            self.visited_list.append(node)
+        print("final", node)
+        self.expansion_grid[node[1], node[2]] = self.step
+a = Search(grid, init, goal, delta, cost, delta_name)
+# print(a.open_list)
+# a.expand([0, 0, 0])
+# print(a.open_list)
 a.compute()
+print(a.expansion_grid)
+print(a.compute_policy())
