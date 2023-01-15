31b84f7b90dd19890add9bea95a3ea33a422cdca
amin-tabrizian <amin_tabrizian@live.com>
2023-01-15 12:52:01 -0500

    A* added

--- part3_11.py
+++ part3_11.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
 init = [0, 0]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 print(goal)
@@ -28,29 +33,35 @@ def is_repeated(node, visited_list):
     return False
 
 class Search:
-    def __init__(self, grid, init, goal, delta, cost, delta_name):
+    def __init__(self, grid, init, goal, delta, cost, delta_name, heuristic):
         self.grid = grid
         self.init = init
         self.goal = goal
         self.delta = delta
         self.cost = cost
         self.delta_name = delta_name
-        self.open_list = [[0, 0, 0]]
+        self.open_list = [[9, 0, 0]]
         self.visited_list = []
         self.expansion_grid = -1*np.ones((len(grid), len(grid[0])))
         self.step = 0
+        self.heuristic = heuristic
     def expand(self, node):
+        expanded = False
         for action in delta:
             i = action[0] + node[1]
             j = action[1] + node[2]
             if is_valid(i, j, self.grid) and not is_repeated([0, i, j], self.visited_list):
-                new = [node[0] + 1, i, j]
+                new = [node[0] + 1 + self.heuristic[i][j], i, j]
                 if not is_repeated(new, self.open_list):
                     self.open_list.append(new)
+                    expanded = True
+                   
+        if expanded:
+            self.expansion_grid[node[1], node[2]] = self.step
+            self.step += 1
         # print("Open list updated:")
         # print(self.open_list)        
-        self.expansion_grid[node[1], node[2]] = self.step
-        self.step += 1
+        
         # print(self.step)
     def compute_policy(self):
         policy = np.zeros((len(self.grid), len(self.grid[0])), dtype=int)
@@ -67,7 +78,7 @@ class Search:
                             policy[i, j] = idx
                             max = self.expansion_grid[new_i, new_j]
         return policy
-    def compute(self, node = [0, 0, 0]):
+    def compute(self, node = [9, 0, 0]):
         self.open_list.remove(node)
         self.visited_list.append(node)
         while  node[1:3] != self.goal:
@@ -94,11 +105,11 @@ class Search:
             node = n_node.copy()
         path[node[0], node[1]] = '*'
         return path
-a = Search(grid, init, goal, delta, cost, delta_name)
+a = Search(grid, init, goal, delta, cost, delta_name, heuristic)
 # print(a.open_list)
 # a.expand([0, 0, 0])
 # print(a.open_list)
 a.compute()
 print(a.expansion_grid)
-print(a.compute_policy())
-print(a.run_policy(a.compute_policy()).decode())
+# print(a.compute_policy())
+# print(a.run_policy(a.compute_policy()).decode())
